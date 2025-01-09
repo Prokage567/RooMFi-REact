361d2a5cd1bc096aeb5732765058de51f5928a3e
justin <yaplegend6@gmail.com>
2025-01-09 10:08:10 +0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+
+export default function BoxBasic() {
+  return (
+    <Box>
+      This Box renders as an HTML section element.
+      <h1>hello world</h1>
+    </Box>
+    
+  );
+}
