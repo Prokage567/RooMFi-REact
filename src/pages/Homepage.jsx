46bc9a209dcd142712360df9ea24a73b4d4fef8d
KimHye-seong <KimHye-seong>
2025-01-24 11:27:16 +0800

    first commit

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,12 +1,16 @@
 import React, { useState } from 'react'
-import { Box } from '@mui/material';
+import head2 from "../assets/images/head2.svg"
+import logo from "../assets/images/logo.svg"
 
 function Homepage() {
     return (
       <>
-      <Box>
-        
-      </Box>
+      <div>
+        <img src={head2}/>
+      </div>
+      <div>
+        <img class="flex flex-row " src={logo}/>
+      </div>
       </>
     )
 }
