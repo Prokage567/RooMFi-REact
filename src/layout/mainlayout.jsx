bb00c32187cedc86858c8d7192008ae7a0a5f6cd
justin <yaplegend6@gmail.com>
2025-01-30 23:44:23 +0800

    fixed the mainlayout

--- src/layout/mainlayout.jsx
+++ src/layout/mainlayout.jsx
@@ -5,9 +5,8 @@ import '../pages/index.css'
 import head from '../assets/images/head.svg'
 import logo from '../assets/images/logo.svg'
 import icon from '../assets/images/homeIcon.svg'
+import icon2 from '../assets/images/icon2.svg'
 import { cn } from "@/lib/utils"
-import { Input } from "@/components/ui/input"
-import { Button } from "@/components/ui/button"
 import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from "@/components/ui/accordion"
 
 export default function MainLayout() {
@@ -24,94 +23,168 @@ export default function MainLayout() {
     const { user, logout } = useContext(AuthContext)
     return (
         <>
-            <div>
-                <div className="shadow-[0_5px_5px_rgba(0,0,0,0.35)] " >
-                    <img src={head} className='min-w-full' />
-                </div>
-                <div className="absolute left-3 top-1 flex flex-row pl-[25px]">
-                    <img src={logo} className='w-[65px] h-[65px]' />
-                    <div className='font-[KronaOne] text-orange-500 text-[40px]/[31.8px] pl-[15px]'>
-                        <br />
-                        RooMFI
+            <div className='flex min-h-screen flex-col overflow-auto'>
+                <nav>
+                    <div className='bg-[url(src/assets/images/head.svg)] bg-cover bg-no-repeat shadow-[0_7px_5px_rgba(0,0,0,0.25)]'>
+                        <div className="md:[200px] flex flex-row pl-[25px] p-3">
+                            <img src={logo} className='w-[85px] md:w-[75px] lg:w-[65px]' />
+                            <div className='font-[KronaOne] text-orange-500 md:text-[50px]/[34.8px] lg:text-[40px]/[31.8px] text-[55px]/[39.8px] pl-[15px]'>
+                                <br />
+                                RooMFI
+                            </div>
+                        </div>
                     </div>
-                </div>
-            </div>
-            <nav className="flex">
-                <div>
-                    <aside className={cn("fixed w-[235px] h-full bg-blue-950 text-white shadow-lg flex flex-col pl-5 pr-8 shadow-[5px_0_10px_rgba(0,0,0,0.35)] ")}>
-                        <nav className="font-[NiramitReg]">
-                            <div className="flex flex-row mt-5 hover:bg-cyan-900">
-                                <img src={icon} height="35px" width="35px" />
-                                <Link to='/' className="no-underline flex pb-[10px] pl-[15px] items-center text-[20px]/[19.4px] font-[NiramitBold] sticky top-0 ">
+                </nav>
+                <div className='flex-1 flex text-white font-[NiramitReg]'>
+                    <nav className="bg-blue-950 shadow-[5px_0_10px_rgba(0,0,0,0.35)]">
+                        <div className=''>
+                            <Link to='/homepage' className="no-underline flex pb-[10px] p-2 items-center text-[20px]/[19.4px] hover:none md:hover:bg-cyan-700 hover:rounded-[10px] lg:hover:bg-cyan-700 hover:rounded-[10px] font-[NiramitBold] sticky top-0 transition-all ">
+                                <img src={icon} className=' lg:w-[55px] p-[3.5px] w-[45px] transition-all hover:bg-cyan-700 hover:rounded-[10px]' />
+                                <div className='hidden lg:block md:block transition-all'>
                                     <br />
                                     Home
-                                </Link>
-                            </div>
-                            <div className='overflow-auto no-scrollbar max-h-screen w-[13vw]'>
-                                <Link to="/room" className="flex flex-col items-center mt-[20px]">
-                                    <Accordion type="single" collapsible className="w-full">
-                                        <AccordionItem value="item-1">
-                                            <AccordionTrigger>Room</AccordionTrigger>
-                                            <AccordionContent>Lecture Rooms</AccordionContent>
-                                            <AccordionContent>Science Laboratories</AccordionContent>
-                                            <AccordionContent>Computer Laboratories</AccordionContent>
-                                            <AccordionContent>Electronics Laboratories</AccordionContent>
-                                            <AccordionContent>Electrical Laboratories</AccordionContent>
-                                            <AccordionContent>Automations & Mechatronics</AccordionContent>
-                                            <AccordionContent>Instrumentations</AccordionContent>
-                                            <AccordionContent>Fiber Optic Laboratory</AccordionContent>
-                                            <AccordionContent>Other Rooms</AccordionContent>
-                                        </AccordionItem>
-                                    </Accordion>
-                                </Link>
-                                <Link to="/section">
-                                    <Accordion type="single" collapsible className="w-full">
-                                        <AccordionItem value="item-2">
-                                            <AccordionTrigger>Section</AccordionTrigger>
-                                            <AccordionContent>CPROG/AUTO</AccordionContent>
-                                            <AccordionContent>STEM</AccordionContent>
-                                            <AccordionContent>1H1</AccordionContent>
-                                            <AccordionContent>1H2</AccordionContent>
-                                            <AccordionContent>1H3</AccordionContent>
-                                            <AccordionContent>IET</AccordionContent>
-                                            <AccordionContent>IAMT</AccordionContent>
-                                            <AccordionContent>DAD</AccordionContent>
-                                            <AccordionContent>DENSO</AccordionContent>
-                                        </AccordionItem>
-                                    </Accordion>
-                                </Link>
+                                </div>
+                            </Link>
+                        </div>
 
-                                <Link to="/teacher" className="flex items-center">
-                                    <Accordion type="single" collapsible className="w-full">
-                                        <AccordionItem value="item-3">
-                                            <AccordionTrigger>Teacher</AccordionTrigger>
-                                            <AccordionContent>CPROG/AUTO</AccordionContent>
-                                            <AccordionContent>STEM</AccordionContent>
-                                            <AccordionContent>1H1</AccordionContent>
-                                            <AccordionContent>1H2</AccordionContent>
-                                            <AccordionContent>1H3</AccordionContent>
-                                            <AccordionContent>IET</AccordionContent>
-                                            <AccordionContent>IAMT</AccordionContent>
-                                            <AccordionContent>DAD</AccordionContent>
-                                            <AccordionContent>DENSO</AccordionContent>
-                                        </AccordionItem>
-                                    </Accordion>
-                                </Link>
-                            </div>
-                        </nav>
-                    </aside>
-                </div>
-
-
-            </nav >
-            <div className='justify-items-center flex'>
-                <div className="flex relative max-w-md mx-auto mt-7 ">
-                    <Input type="text" placeholder="Search room..." value={search}
-                        className={"h-[45px] border-2 border-blue-950 pl-10 pr-4 py-2 border-2 rounded-full w-[500px] focus:border-[1] "} />
-                    <Button type="submit">Search</Button>
+                        <div className="overflow-auto no-scrollbar h-[100vh]">
+                            <Accordion type="single" collapsible>
+                                <AccordionItem value="item-1">
+                                    <Link to="/room" className="flex flex-col ">
+                                    <AccordionTrigger img={icon2}>Room</AccordionTrigger>
+                                    </Link>
+                                    <AccordionContent>Lecture Rooms</AccordionContent>
+                                    <AccordionContent>Science Laboratories</AccordionContent>
+                                    <AccordionContent>Computer Laboratories</AccordionContent>
+                                    <AccordionContent>Electronics Laboratories</AccordionContent>
+                                    <AccordionContent>Electrical Laboratories</AccordionContent>
+                                    <AccordionContent>Automations & Mechatronics</AccordionContent>
+                                    <AccordionContent>Instrumentations</AccordionContent>
+                                    <AccordionContent>Fiber Optic Laboratory</AccordionContent>
+                                    <AccordionContent>Other Rooms</AccordionContent>
+                                </AccordionItem>
+                            </Accordion>
+                            <Accordion type="single" collapsible>
+                                <AccordionItem value="item-2">
+                                    <Link to="/room" className="flex flex-col ">
+                                    <AccordionTrigger img={icon2}>Room</AccordionTrigger>
+                                    </Link>
+                                    <AccordionContent>Lecture Rooms</AccordionContent>
+                                    <AccordionContent>Science Laboratories</AccordionContent>
+                                    <AccordionContent>Computer Laboratories</AccordionContent>
+                                    <AccordionContent>Electronics Laboratories</AccordionContent>
+                                    <AccordionContent>Electrical Laboratories</AccordionContent>
+                                    <AccordionContent>Automations & Mechatronics</AccordionContent>
+                                    <AccordionContent>Instrumentations</AccordionContent>
+                                    <AccordionContent>Fiber Optic Laboratory</AccordionContent>
+                                    <AccordionContent>Other Rooms</AccordionContent>
+                                </AccordionItem>
+                            </Accordion>
+                            <Accordion type="single" collapsible>
+                                <AccordionItem value="item-3">
+                                    <Link to="/room" className="flex flex-col ">
+                                    <AccordionTrigger img={icon2}>Room</AccordionTrigger>
+                                    </Link>
+                                    <AccordionContent>Lecture Rooms</AccordionContent>
+                                    <AccordionContent>Science Laboratories</AccordionContent>
+                                    <AccordionContent>Computer Laboratories</AccordionContent>
+                                    <AccordionContent>Electronics Laboratories</AccordionContent>
+                                    <AccordionContent>Electrical Laboratories</AccordionContent>
+                                    <AccordionContent>Automations & Mechatronics</AccordionContent>
+                                    <AccordionContent>Instrumentations</AccordionContent>
+                                    <AccordionContent>Fiber Optic Laboratory</AccordionContent>
+                                    <AccordionContent>Other Rooms</AccordionContent>
+                                </AccordionItem>
+                            </Accordion>
+                        </div>
+                    </nav>
+                    <main>
+                        <Outlet />
+                    </main>
                 </div>
             </div>
-            <Outlet />
+
+
+
+
+
+            {/* <div className='flex min-h-screen flex-col'>
+                <nav>
+                    
+                    
+                </nav>
+
+                <nav className="flex">
+                    <div>
+                        <aside className={cn("fixed w-[235px] h-full bg-blue-950 text-white shadow-lg flex flex-col pl-5 pr-8 shadow-[5px_0_10px_rgba(0,0,0,0.35)] ")}>
+                            <nav className="font-[NiramitReg]">
+                                <div className="flex flex-row mt-5 hover:bg-cyan-900">
+                                   
+                                    
+                                </div>
+                                <div className='overflow-auto no-scrollbar max-h-screen w-[13vw]'>
+                                    <Link to="/room" className="flex flex-col items-center mt-[20px]">
+                                        <Accordion type="single" collapsible className="w-full">
+                                            <AccordionItem value="item-1">
+                                                <AccordionTrigger>Room</AccordionTrigger>
+                                                <AccordionContent>Lecture Rooms</AccordionContent>
+                                                <AccordionContent>Science Laboratories</AccordionContent>
+                                                <AccordionContent>Computer Laboratories</AccordionContent>
+                                                <AccordionContent>Electronics Laboratories</AccordionContent>
+                                                <AccordionContent>Electrical Laboratories</AccordionContent>
+                                                <AccordionContent>Automations & Mechatronics</AccordionContent>
+                                                <AccordionContent>Instrumentations</AccordionContent>
+                                                <AccordionContent>Fiber Optic Laboratory</AccordionContent>
+                                                <AccordionContent>Other Rooms</AccordionContent>
+                                            </AccordionItem>
+                                        </Accordion>
+                                    </Link>
+                                    <Link to="/section">
+                                        <Accordion type="single" collapsible className="w-full">
+                                            <AccordionItem value="item-2">
+                                                <AccordionTrigger>Section</AccordionTrigger>
+                                                <AccordionContent>CPROG/AUTO</AccordionContent>
+                                                <AccordionContent>STEM</AccordionContent>
+                                                <AccordionContent>1H1</AccordionContent>
+                                                <AccordionContent>1H2</AccordionContent>
+                                                <AccordionContent>1H3</AccordionContent>
+                                                <AccordionContent>IET</AccordionContent>
+                                                <AccordionContent>IAMT</AccordionContent>
+                                                <AccordionContent>DAD</AccordionContent>
+                                                <AccordionContent>DENSO</AccordionContent>
+                                            </AccordionItem>
+                                        </Accordion>
+                                    </Link>
+
+                                    <Link to="/teacher" className="flex items-center">
+                                        <Accordion type="single" collapsible className="w-full">
+                                            <AccordionItem value="item-3">
+                                                <AccordionTrigger>Teacher</AccordionTrigger>
+                                                <AccordionContent>CPROG/AUTO</AccordionContent>
+                                                <AccordionContent>STEM</AccordionContent>
+                                                <AccordionContent>1H1</AccordionContent>
+                                                <AccordionContent>1H2</AccordionContent>
+                                                <AccordionContent>1H3</AccordionContent>
+                                                <AccordionContent>IET</AccordionContent>
+                                                <AccordionContent>IAMT</AccordionContent>
+                                                <AccordionContent>DAD</AccordionContent>
+                                                <AccordionContent>DENSO</AccordionContent>
+                                            </AccordionItem>
+                                        </Accordion>
+                                    </Link>
+                                </div>
+                            </nav>
+                        </aside>
+                    </div>
+
+
+                </nav >
+
+
+
+            </div>
+
+            <Outlet /> */}
         </>
     )
 }
--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,10 +1,29 @@
 import React, { useState } from 'react'
 import { Box } from '@mui/material';
-import withAuth from '../highOrdeerComponent/withAuth';
+import withAuth from '../highOrdeerComponent/withAuth'
+import { Input } from "@/components/ui/input"
+import { Button } from "@/components/ui/button"
+import image from '../assets/images/image.svg'
+
 function Homepage() {
     return (
       <>
       <Box>
+      <div className='justify-items-center flex'>
+                <div className="flex relative max-w-md mx-auto mt-7 ">
+                    <Input type="text" placeholder="Search room..."
+                        className={"h-[45px] border-2 border-blue-950 pl-10 pr-4 py-2 border-2 rounded-full w-[500px] focus:border-[1] "} />
+                    <Button type="submit">Search</Button>
+                </div>
+            </div>
+
+            <div className='ml-[150px]'>
+                <div className='float-left w-[60%]'>
+                    MFI’s Room Management System: Room Scheduling and Individual Tracking
+                </div>
+            </div>
+            <img src={image} alt="" width="500px" height="500px" className='float-right w-[40%] pr-[100px]' />
+
       </Box>
       </>
     )
