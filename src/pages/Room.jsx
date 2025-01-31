591bf0c0e57d86646872be8292e2531a794a8c7c
KimHye-seong <KimHye-seong>
2025-01-31 13:02:54 +0800

    room assigned by elgave

--- src/pages/Room.jsx
+++ src/pages/Room.jsx
@@ -1,9 +1,28 @@
-export default function Room(){
+import * as React from "react"
+ 
+import { AspectRatio } from "@/components/ui/aspect-ratio"
+
+function Room(){
 
     return(
-        <>  <div className="justify-center flex">
-        jhsFgawf
+        <>  
+        
+        <div className="justify-center flex">
+            <div className="pl-10 pt-5 text-[45px] text-[#0F1A42]">
+                Lecture Rooms
+            </div>
+            <div>
+                <AspectRatio ratio={16/9} className="bg-muted">
+                    <img 
+                        src="" 
+                        alt="" 
+                        className=""
+                    />
+                </AspectRatio>
+            </div>
+
         </div>
         </>
     )
 }
+export default Room
