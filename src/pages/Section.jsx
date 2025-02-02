438b4a8e74fc4012ae7448eedc7c0179eb2a1e3e
Clare Lubiano <princessclare7177@gmail.com>
2025-02-03 05:33:32 +0800

    working on section

--- src/pages/Section.jsx
+++ src/pages/Section.jsx
@@ -6,14 +6,13 @@ import Add from "../assets/images/add.svg"
 import {
   Dialog,
   DialogContent,
-  DialogHeader,
-  DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog"
 import { Input} from "../components/ui/input"
 import {Label} from "../components/ui/label"
 import {Checkbox} from "../components/ui/checkbox"
 import { Button } from "../components/ui/button"
+// import {PopUpCalendar as PopUp} from "../components/popUpCalendar"
 
 
 
@@ -31,42 +30,43 @@ export default function section(){
         />
         </div>
 
-        <Dialog className="rounded-full" >
+        <Dialog className="rounded-full w-[500px]" >
           <DialogTrigger>
             <img src={Add} className="w-[50px] h-[50px] mr-[10px] mb-[10px] fixed bottom-0 right-0" /> 
           </DialogTrigger>
 
           <DialogContent className="bg-slate-900 border-none text-[#fff]">
-            <DialogHeader>
-              <DialogTitle className=" font-thin font-[NiramitReg]">Add an Event</DialogTitle>
-            </DialogHeader>
 
             <div className="grid  max-w-sm items-center gap-1.5 ml-3 w-[390px] font-[NiramitReg]">
-              <Label htmlFor="email">Room NO.</Label>
+              <Label className="text-[20px]">Room NO.</Label>
               <Input  autofocus e={false} 
-                      className="focus:outline-double" 
+                      className="focus:outline-double h-10 placeholder:text-[18px] text-[20px] " 
                       type="number" 
                       id="room.no" 
                       placeholder="Input Room Number" />
 
-              <Label htmlFor="email">Teacher Name</Label>
+              <Label className="text-[20px]">Teacher Name</Label>
               <Input autofocus e={false} 
                      id="teacher_name"
+                     className="h-10 placeholder:text-[18px] text-[20px]"
                      placeholder="Type or Select a Teacher" />
 
-              <Label htmlFor="email">Subject</Label>
+              <Label className="text-[20px]">Subject</Label>
               <Input autofocus e={false}  
+                     className="h-10 placeholder:text-[18px] text-[20px]"
                      id="subject" 
                      placeholder="Input Subject" />
+              
+              {/* <PopUp  /> */}
 
-              <div className="grid grid-flow-col w-[450px] h-[150px] gap-2 ">
+              <div className="grid grid-flow-col w-[450px] mt-[15px] h-[150px] gap-2 ">
 
                 <div className=" w-[150px] row-span-3" >
-                  <Label className="ml-10">Time</Label><br/>
-                  <Label >From:</Label>
+                  <Label className="ml-10 text-[20px]">Time</Label><br/>
+                  <Label className="text-[18px]" >From:</Label>
                   <Input className="border-none focus:outline-white"  type="time"/>
-                  <Label>To:</Label>
-                  <Input className="border-none" type="time"/>
+                  <Label className="text-[18px]">To:</Label>
+                  <Input className="border-none " type="time"/>
                 </div>
 
                 <div className="col-span-2 w-[270px]  h-[110px]  mt-2 ">
@@ -75,7 +75,7 @@ export default function section(){
                     <div className="grid gap-1.5 leading-none">
                       <label
                         htmlFor="application"
-                        className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70">
+                        className="font-medium leading-none peer-disabled:cursor-not-allowed text-[16px] peer-disabled:opacity-70">
                         Apply to all week days? (monday)
                       </label>
                     </div>
@@ -83,7 +83,7 @@ export default function section(){
                 </div>
 
                 <div className="col-span-2 row-span-2 ml-[210px]">
-                  <Button> save </Button>
+                  <Button className="hover:font-extrabold hover:bg-transparent font-[10] font-[NiramitReg] bg-transparent text-[20px]"> Save </Button>
                 </div>
 
               </div>
