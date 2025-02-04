74dabc920087b1c5ea9ee24d9434e7c00cd2d471
Clare Lubiano <princessclare7177@gmail.com>
2025-02-04 09:52:46 +0800

    wahhhh

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,6 @@
 import path from "path"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
-<<<<<<< HEAD
- 
-=======
-
->>>>>>> ae5281432a1d30dbce135d76e425b1ebb6e12fd8
 export default defineConfig({
   plugins: [react()],
   resolve: {
@@ -13,8 +8,4 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-<<<<<<< HEAD
 })
-=======
-})
->>>>>>> ae5281432a1d30dbce135d76e425b1ebb6e12fd8
