9a76b44eaf1290eda0c3d9b3f7e475ba387d98eb
justin <yaplegend6@gmail.com>
2025-02-04 14:48:38 +0800

    update in hoempage and main layout

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -11,27 +11,27 @@ function Homepage() {
     return (
         <>
             {/* ignore this yet */}
-            <div className="flex flex-col items-center overflow-y-auto no-scrollbar md:max-h-[90vh] lg:max-h-[120vh]">
-                <div className='pl-[365px] pr-[365px] flex sticky top-9 flex-row'>
+            <div className="h-full max-w-screen-2xl">
+                <div className=' items-center justify-center pl-[365px]pr-[365px] flex sticky top-9 flex-row'>
                     <Input symbol={true} type="text" placeholder="Search room..."
                         className="h-[45px] border-2 bg-slate border-blue-950 pl-10 pr-4 py-2 rounded-full w-[500px] focus:border-[1] " />
                     <Button type="submit">Search</Button>
                 </div>
-                <div className='flex pt-[50px] flex-row md:pl-[100px] pb-12 lg:pl-[100px] pr-[50px] lg:w-[28vw]'>
+                <div className='flex pt-[50px] flex-row md:pl-[100px] pb-12 lg:pl-[100px] pr-[50px] w-fit'>
                     <div className='pt-[100px]'>
                         <p className='font-[NiramitReg] text-[17px]'>MFI’s Room Management System: Room Scheduling and Individual Tracking
                         </p>
                         <h4 className='font-[KronaOne] text-[40px] pt-6'>
                             RooMFI
-                        </h4><p className='font-[NiramitReg] text-[25px] -leading-1 pr-5'>
+                        </h4><p className='font-[NiramitReg] text-[25px] -leading-1 pr-5 w-[560px]'>
                             A Smooth and organized system where MFIers can efficiently manage or view room schedules, along with a tracker providing information on the sections and teachers assigned to each room.
                         </p>
                     </div>
                     <img src={img} alt="" width='520px' className='pt-[20px] mr-[70px]' />
                 </div>
-                <div className='flex flex-col w-full'>
+                <div className='flex flex-col '>
                     <img src={img2} alt="" className='pt-[7vh] -z-10' />
-                    <div className='pl-[650px] relative -z-10 md:-top-[450px] md:-mb-[450px] lg:-top-[650px] lg:left-[450px]'>
+                    <div className='pl-[650px] relative -z-10 -top-[450px] -mb-[450px]'>
                         <div className='h-[550px] w-[470px] pt-[40px] bg-cyan-500 text-zinc-100 p-12'>
                             <h2 className='font-[KronaOne] text-[20px] pl-[20px] pr-[50px]'>
                                 Room Management & Individual Tracks
@@ -49,7 +49,7 @@ function Homepage() {
                             </ul>
                         </div>
                     </div>
-                    <img className='w-full' src={foot} alt="" />
+                    <img src={foot} alt="" />
                 </div>
             </div>
         </>
